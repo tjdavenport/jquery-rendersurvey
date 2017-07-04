93d5b54626cb8656038afe440e1ad33440e8bf8e
Torrance Davenport <tjdavenport2@gmail.com>
2017-07-03 21:22:15 -0400

    more styles, validation and toggling

--- jquery.renderSurvey.js
+++ jquery.renderSurvey.js
@@ -1,40 +1,87 @@
 (function($) {
   $.fn.renderSurvey = function(options) {
     const $form = $(`
-      <form>
-        <h1 class="jq-rs-title"></h1>
+      <form method="POST" action="${options.submitUrl}">
+        <h1 class="jq-rs-title">${options.question}</h1>
         <div data-contain="answers"></div>
         <div style="clear:both;"></div>
         <div data-contain="other">
-          <div class="jq-rs-pill jq-rs-pill-centered">
+          <div class="jq-rs-pill jq-rs-centered">
             <table>
               <tr>
                 <td>
                   <label>Other:</label>
                 </td>
                 <td style="width:100%;padding-left:10px;padding-right:15px;">
-                  <input type="text" name="other">
+                  <input type="text" name="other_answer">
                 </td>
               </tr>
             </table>
           </div>
         </div>
+        <div data-contain="submit">
+          <button disabled="disabled" class="jq-rs-btn jq-rs-centered" type="submit">Submit</button>
+        </div>
       </form>
     `);
     const $answer = $(`
-      <div data-contain="answer" class="jq-rs-pill jq-rs-pill-floated">
+      <div data-value="" data-selected="0" data-contain="answer" class="jq-rs-pill jq-rs-pill-floated">
         <label></label>
-        <input type="hidden" name="answer">
       </div>
     `);
+    const $selected = $(`<div data-role="selected" class="jq-rs-selected"></div>`);
 
-    $form.find('.jq-rs-title').text(options.question);
+    function formIsValid() {
+      const $selected = $form.find('[data-selected="1"]');
+      const $other = $form.find('[data-contain="other"] [name="other_answer"]');
+      const atLeastOneSelect = !!$selected.length;
+      const otherSpecified = !!$other.val();
+
+      if (atLeastOneSelect || otherSpecified) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function toggleForm(enabled) {
+      if (enabled) {
+        $form.find('[data-contain="submit"] [type="submit"]').removeAttr('disabled');
+      } else {
+        $form.find('[data-contain="submit"] [type="submit"]').attr('disabled', 'disabled');
+      }
+    }
 
     $.each(options.answers, (index, answer) => {
-      $answerClone = $answer.clone();
+      const $answerClone = $answer.clone();
+
+      $answerClone.click((e) => {
+        const selected = !!parseInt($answerClone.attr('data-selected'));
+        const value = $answerClone.attr('data-value');
+
+        if (selected) {
+          $answerClone.find('[data-role="selected"]').remove();
+          $answerClone.find('input').remove();
+          $answerClone.attr('data-selected', 0);
+        } else {
+          let $input = $(`<input type="hidden" name="answers[]" value="${value}">`);
+          $answerClone.append($selected.clone());
+          $answerClone.append($input);
+          $answerClone.attr('data-selected', 1);
+        }
+
+        toggleForm(formIsValid());
+      });
+
+      $answerClone.attr('data-value', answer);
       $answerClone.find('label').text(answer);
       $answerClone.find('[name="answer"]').val(answer);
       $form.find('[data-contain="answers"]').append($answerClone);
+      $form.attr('')
+    });
+
+    $form.find('[data-contain="other"] [name="other_answer"]').on('input', () => {
+      toggleForm(formIsValid());
     });
 
     this.append($form);
